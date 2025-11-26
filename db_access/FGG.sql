80c91cf1f686c7ccc11cd43ab8e20d4cefbb59bb
Jegern <severunov@gmail.com>
2025-11-27 00:43:46 +0500

    Added a new database scheme.

--- db_access/FGG.sql
+++ db_access/FGG.sql
@@ -1,122 +1,169 @@
 --
--- File generated with SQLiteStudio v3.4.17 on Sat Nov 8 22:29:34 2025
+-- File generated with SQLiteStudio v3.4.17 on Thu Nov 27 00:43:14 2025
 --
 -- Text encoding used: System
 --
 PRAGMA foreign_keys = off;
 BEGIN TRANSACTION;
 
+-- Table: AvailableRolls
+CREATE TABLE IF NOT EXISTS AvailableRolls (
+    Id         INTEGER PRIMARY KEY AUTOINCREMENT
+                       UNIQUE
+                       NOT NULL,
+    UserId     INTEGER REFERENCES Users (Id) 
+                       NOT NULL,
+    CreateDate TEXT    NOT NULL
+                       DEFAULT (datetime('now', 'subsec') ) 
+);
+
+
+-- Table: BonusPointsHistory
+CREATE TABLE IF NOT EXISTS BonusPointsHistory (
+    Id INTEGER UNIQUE
+             PRIMARY KEY AUTOINCREMENT
+             NOT NULL
+);
+
+
 -- Table: EffectHistory
 CREATE TABLE IF NOT EXISTS EffectHistory (
-    Id             TEXT    PRIMARY KEY
-                           UNIQUE
-                           NOT NULL,
-    UserId         TEXT    REFERENCES Users (Id) 
-                           NOT NULL,
-    GameId         TEXT    REFERENCES Games (Id),
-    CreateDate     TEXT    NOT NULL
-                           DEFAULT (datetime('now', 'subsec') ),
-    RollDate       TEXT,
-    RolledEffectId INTEGER REFERENCES Effects (Id) 
+    Id         INTEGER PRIMARY KEY AUTOINCREMENT
+                       NOT NULL
+                       UNIQUE,
+    UserId     INTEGER REFERENCES Users (Id) 
+                       NOT NULL,
+    EffectId   INTEGER REFERENCES Effects (Id) 
+                       NOT NULL,
+    CreateDate TEXT    NOT NULL
+                       DEFAULT (datetime('now', 'subsec') ) 
 );
 
 
 -- Table: Effects
 CREATE TABLE IF NOT EXISTS Effects (
-    Id          TEXT PRIMARY KEY
-                     NOT NULL
-                     UNIQUE,
-    Name        TEXT NOT NULL,
-    Description TEXT NOT NULL
+    Id                      INTEGER PRIMARY KEY AUTOINCREMENT
+                                    UNIQUE
+                                    NOT NULL,
+    Name                    TEXT    NOT NULL,
+    Description             TEXT    NOT NULL,
+    IsCompleted             INTEGER DEFAULT (0) 
+                                    NOT NULL,
+    OwnerPointChangeFormula TEXT    NOT NULL
+                                    DEFAULT ('output 0'),
+    EffectRerollFormula     TEXT    NOT NULL
+                                    DEFAULT ('output 0'),
+    IsItem                  INTEGER DEFAULT (0) 
+                                    NOT NULL,
+    IsEffectChoice          INTEGER NOT NULL
+                                    DEFAULT (0),
+    RepeatCount             INTEGER NOT NULL
+                                    DEFAULT (1) 
 );
 
 
 -- Table: GameHistory
 CREATE TABLE IF NOT EXISTS GameHistory (
-    Id           TEXT    PRIMARY KEY
-                         NOT NULL
-                         UNIQUE,
-    UserId       TEXT    REFERENCES Users (Id) 
-                         NOT NULL,
-    GameId       TEXT    REFERENCES Games (Id) 
-                         NOT NULL,
-    State        TEXT    NOT NULL
-                         DEFAULT ('started'),
-    CreateDate   TEXT    NOT NULL
-                         DEFAULT (datetime('now', 'subsec') ),
-    ResultPoints INTEGER,
-    FinishDate   TEXT
+    Id         INTEGER PRIMARY KEY AUTOINCREMENT
+                       NOT NULL
+                       UNIQUE,
+    UserId     INTEGER REFERENCES Users (Id) 
+                       NOT NULL,
+    GameId     INTEGER NOT NULL
+                       REFERENCES Games (Id),
+    State      TEXT    NOT NULL
+                       DEFAULT ('started'),
+    CreateDate TEXT    NOT NULL
+                       DEFAULT (datetime('now', 'subsec') ),
+    MapPoints  INTEGER,
+    FinishDate TEXT
 );
 
 
 -- Table: Games
 CREATE TABLE IF NOT EXISTS Games (
-    Id         TEXT PRIMARY KEY
-                    UNIQUE
-                    NOT NULL,
-    Name       TEXT NOT NULL
-                    UNIQUE,
-    Link       TEXT,
-    CreateDate TEXT DEFAULT (datetime('now', 'subsec') ) 
-                    NOT NULL
+    Id         INTEGER PRIMARY KEY AUTOINCREMENT
+                       UNIQUE
+                       NOT NULL,
+    Name       TEXT    NOT NULL
+                       UNIQUE,
+    Link       TEXT    UNIQUE,
+    CreateDate TEXT    DEFAULT (datetime('now', 'subsec') ) 
+                       NOT NULL
 );
 
 
 -- Table: TimerActions
 CREATE TABLE IF NOT EXISTS TimerActions (
-    Id               TEXT    PRIMARY KEY
+    Id               INTEGER PRIMARY KEY AUTOINCREMENT
                              UNIQUE
                              NOT NULL,
-    TimerId          TEXT    REFERENCES Timers (Id) 
+    TimerId          INTEGER REFERENCES Timers (Id) 
                              NOT NULL,
     Action           TEXT    NOT NULL,
-    Date             TEXT    NOT NULL
+    CreateDate       TEXT    NOT NULL
                              DEFAULT (datetime('now', 'subsec') ),
-    RemainingTimeInS INTEGER
+    RemainingTimeInS INTEGER NOT NULL
 );
 
 
 -- Table: Timers
 CREATE TABLE IF NOT EXISTS Timers (
-    Id          TEXT    PRIMARY KEY
+    Id          INTEGER PRIMARY KEY AUTOINCREMENT
                         UNIQUE
                         NOT NULL,
-    UserId      TEXT    REFERENCES Users (Id) 
+    UserId      INTEGER REFERENCES Users (Id) 
                         NOT NULL,
-    GameId      TEXT    REFERENCES Games (Id) 
-                        NOT NULL,
+    GameId      INTEGER NOT NULL
+                        REFERENCES Games (Id),
     State       TEXT    NOT NULL
                         DEFAULT ('created'),
-    DurationInS INTEGER NOT NULL
-                        DEFAULT (10800),
-    CreateDate          NOT NULL
+    DurationInS INTEGER NOT NULL,
+    CreateDate  TEXT    NOT NULL
                         DEFAULT (datetime('now', 'subsec') ) 
 );
 
 
 -- Table: UnplayedGames
 CREATE TABLE IF NOT EXISTS UnplayedGames (
-    Id         TEXT PRIMARY KEY
-                    UNIQUE
-                    NOT NULL,
-    UserId     TEXT REFERENCES Users (Id) 
-                    NOT NULL,
-    GameId     TEXT REFERENCES Games (Id) 
-                    NOT NULL,
-    CreateDate      NOT NULL
-                    DEFAULT (datetime('now', 'subsec') ) 
+    Id         INTEGER PRIMARY KEY AUTOINCREMENT
+                       UNIQUE
+                       NOT NULL,
+    UserId     INTEGER REFERENCES Users (Id) 
+                       NOT NULL,
+    GameId     INTEGER NOT NULL
+                       REFERENCES Games (Id),
+    CreateDate TEXT    NOT NULL
+                       DEFAULT (datetime('now', 'subsec') ) 
 );
 
 
 -- Table: Users
 CREATE TABLE IF NOT EXISTS Users (
-    Id       TEXT PRIMARY KEY
-                  UNIQUE
-                  NOT NULL,
-    Name     TEXT NOT NULL
-                  UNIQUE,
-    JoinDate TEXT DEFAULT (datetime('now', 'subsec') ) 
-                  NOT NULL
+    Id       INTEGER PRIMARY KEY AUTOINCREMENT
+                     UNIQUE
+                     NOT NULL,
+    Name     TEXT    NOT NULL
+                     UNIQUE,
+    Email            UNIQUE
+                     NOT NULL,
+    Password TEXT    NOT NULL,
+    JoinDate TEXT    DEFAULT (datetime('now', 'subsec') ) 
+                     NOT NULL
+);
+
+
+-- Table: UserSessions
+CREATE TABLE IF NOT EXISTS UserSessions (
+    Id         TEXT    PRIMARY KEY
+                       UNIQUE
+                       NOT NULL,
+    UserId     INTEGER REFERENCES Users (Id) 
+                       NOT NULL,
+    CreateDate TEXT    NOT NULL
+                       DEFAULT (datetime('now', 'subsec') ),
+    ExpiryDate TEXT    NOT NULL
+                       DEFAULT (datetime('now', 'subsec', '+1 day') ) 
 );
 
 
