84be5d5fcf93c014f39d173ac3f3880e84ed1951
Dmitry Severyanov <severunov@gmail.com>
2026-02-23 21:53:16 +0500

    Entrypoint fix.

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,7 @@ set -e
 mkdir -p /app/data
 
 if [ ! -f /app/data/FGG.db ]; then
-  echo "Initializing database..."
-  sqlite3 /app/data/FGG.db < /app/db_access/FGG.sql
+  sqlite3 /app/data/FGG.db < /app/dbaccess/FGG.sql
 fi
 
 exec "$@"
