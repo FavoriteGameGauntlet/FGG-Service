6374bcf9667a2c3b1697846294c04414c926f4fc
Dmitry Severyanov <severunov@gmail.com>
2026-04-06 16:23:22 +0500

    Added an implementation of the endpoint `POST /users/display-name`.

--- src/users/controller/ctrlusers.go
+++ src/users/controller/ctrlusers.go
@@ -1,18 +1,26 @@
 package ctrlusers
 
 import (
+	genusers "FGG-Service/api/generated/users"
+	srvauth "FGG-Service/src/auth/service"
+	"FGG-Service/src/common"
 	"FGG-Service/src/users/service"
+	"FGG-Service/src/validator"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
 )
 
 type Controller struct {
-	Service srvusers.Service
+	Service     srvusers.Service
+	AuthService srvauth.Service
 }
 
 func NewController() *Controller {
-	return new(Controller)
+	s := srvusers.NewService()
+	as := new(srvauth.Service)
+
+	return &Controller{*s, *as}
 }
 
 // GetAllUserNames (GET /users/all/names)
@@ -27,5 +35,31 @@ func (c *Controller) GetDisplayName(ctx echo.Context) error {
 
 // ChangeDisplayName (POST /users/display-name)
 func (c *Controller) ChangeDisplayName(ctx echo.Context) error {
-	return ctx.NoContent(http.StatusNotImplemented)
+	userId, err := c.AuthService.GetUserId(ctx)
+
+	if err != nil {
+		return common.SendJSONErrorResponse(ctx, err)
+	}
+
+	var changeName genusers.ChangeName
+	err = ctx.Bind(&changeName)
+
+	if err != nil {
+		err = common.NewBadRequestError(err.Error())
+		return common.SendJSONErrorResponse(ctx, err)
+	}
+
+	err = validator.ValidateName(changeName.Name)
+
+	if err != nil {
+		return common.SendJSONErrorResponse(ctx, err)
+	}
+
+	err = c.Service.ChangeDisplayName(userId, changeName.Name)
+
+	if err != nil {
+		return common.SendJSONErrorResponse(ctx, err)
+	}
+
+	return ctx.NoContent(http.StatusNoContent)
 }
